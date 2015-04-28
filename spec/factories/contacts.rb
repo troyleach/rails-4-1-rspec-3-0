67973f71a7c2f145767eb2aabf70c51dd2bace90
Troy Leach <troyleach29@gmail.com>
2015-04-28 13:29:22 -0500

    factories

---
+++ spec/factories/contacts.rb
@@ -0,0 +1,22 @@
+FactoryGirl.define do 
+  factory :contact do
+    firstname { Faker::Name.first_name }
+    lastname { Faker::Name.last_name }
+    email { Faker::Internet.email }
+
+    after(:build) do |contact|
+      [:home_phone, :work_phone, :mobile_phone].each do |phone|
+        contact.phones << FactoryGirl.build(:phone,
+          phone_type: phone, contact: contact)
+      end
+    end
+  end
+end
+
+# FactoryGirl.define do 
+#   factory :contact do
+#     firstname "John"
+#     lastname "Doe"
+#     sequence(:email) { |n| "johndo#{n}@example.com" }
+#   end
+# end
---
+++ spec/factories/phones.rb
@@ -0,0 +1,21 @@
+FactoryGirl.define do
+
+  factory :phone do
+    association :contact
+    phone { Faker::PhoneNumber.phone_number }
+    # child factories omitted ...
+
+    factory :home_phone do
+      phone_type 'home'
+    end
+
+    factory :work_phone do 
+      phone_type 'work'
+    end
+
+    factory :mobile_phone do
+      phone_type 'mobile'
+    end
+  end
+  
+end
